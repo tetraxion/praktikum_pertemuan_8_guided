07b66077bd617af5f6c4d02b7b2ae6213babb993
Tetraxion <lutfid955@gmail.com>
2023-11-06 19:49:53 +0700

    fist commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
           children: [
             HorizontalDrag(),
             Divider(),
-//VerticalDrag(),
+            VerticalDrag(),
           ],
         ),
       ),
